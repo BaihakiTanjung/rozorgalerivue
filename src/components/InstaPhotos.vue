616abe779c3d56aa6a45c9b8f43a9b7e03b9eec4
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2020-04-05 16:48:19 +0700

    Upload Projek
    Upload Projek

---
+++ src/components/InstaPhotos.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- Instagram Section Begin -->
+    <div class="instagram-photo">
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-1.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-2.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-3.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-4.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-5.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+      <div class="insta-item set-bg" style="background-image : url('/img/insta-6.jpg')">
+        <div class="inside-text">
+          <i class="ti-instagram"></i>
+          <h5>
+            <a href="#">rozor_gallery</a>
+          </h5>
+        </div>
+      </div>
+    </div>
+    <!-- Instagram Section End -->
+  </div>
+</template>
+<script>
+export default {
+  name: "InstaPhotos"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <HeroCarousel></HeroCarousel>
+    <SliderCarousel></SliderCarousel>
+    <InstaPhotos></InstaPhotos>
+    <PartnerLogos></PartnerLogos>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+import HeroCarousel from "@/components/HeroCarousel.vue";
+import SliderCarousel from "@/components/SliderCarousel.vue";
+import InstaPhotos from "@/components/InstaPhotos.vue";
+import PartnerLogos from "@/components/PartnerLogos.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    Header,
+    HeroCarousel,
+    SliderCarousel,
+    InstaPhotos,
+    PartnerLogos,
+    Footer
+  }
+};
+</script>
